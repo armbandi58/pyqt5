c23ba5bd3c652a0e9f7467a95e8a6d527f5a7919
borsa <borsandras@outlook.hu>
2021-05-02 18:40:34 +0200

    update

--- pyqt5/test.py
+++ pyqt5/test.py
@@ -11,7 +11,13 @@ class MainWindow(qtw.QWidget):
         #label
         my_label = qtw.QLabel("Tulajdonkeppen megy")
         my_label.setFont(qtg.QFont('Helvetica',18))
+
+        my_label2 = qtw.QLabel("Masik")
+        my_label2.setFont(qtg.QFont('Helvetica',20))
+
         self.layout().addWidget(my_label)
+        self.layout().addWidget(my_label2)
+
         #Entry box
         my_entry = qtw.QLineEdit()
         my_entry.setObjectName("name_field")
@@ -21,6 +27,15 @@ class MainWindow(qtw.QWidget):
         my_button = qtw.QPushButton("Nem mered",clicked = lambda:nyomva())
         self.layout().addWidget(my_button)
 
+        my_button2 = qtw.QPushButton("Masik", clicked = lambda: lista())
+        self.layout().addWidget(my_button2)
+
+        #Combo box
+        combo_box0 = qtw.QComboBox(self)
+        combo_box0.addItems(["Chose0", "Chose1", "Chose2", "Chose3"])
+        self.layout().addWidget(combo_box0)
+
+
         #mutasd az appot
         self.show()
 
@@ -29,12 +44,14 @@ class MainWindow(qtw.QWidget):
             my_label.setText(my_entry.text())
             my_entry.setText("")
 
+        def lista():
+            my_label2.setText( f'You choose this: {combo_box0.currentIndex()}')
+
 string_title = "Ez lehetne a cim"
 
-app = qtw.QApplication([])
-mw = MainWindow(string_title)
-
 if __name__ == "__main__":
+    app = qtw.QApplication([])
+    mw = MainWindow(string_title)
     app.exec_()
 
 
